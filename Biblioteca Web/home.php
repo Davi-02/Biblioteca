9bc373e50ba50fa56eb25ef0917a06ceab5255d5
Davi-02 <120059108+Davi-02@users.noreply.github.com>
2023-07-04 09:14:06 -0300

    Add files via upload

---
+++ Biblioteca Web/home.php
@@ -0,0 +1,22 @@
+<? php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+</head>
+<body>
+	<header class="header">
+		
+	</header>
+	<main class="main">
+		
+	</main>
+	<footer class="footer">
+		
+	</footer>
+</body>
+</html>
